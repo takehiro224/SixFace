50be9bcbecc7da2f10874e7498805e3380df4408
takehiro <takehiro.watanabe@globis.co.jp>
2019-05-17 18:02:39 +0900

    Initial Commit

---
+++ SixFace/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SixFace
+//
+//  Created by Watanabe Takehiro on 2019/05/17.
+//  Copyright © 2019 Watanabe Takehiro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
